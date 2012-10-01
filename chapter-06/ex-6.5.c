c4e9ae2f8c06df4c00ab7d8f9c938cf4618de69d
jaseemjas <jasmedia.jas@gmail.com>
2012-10-01 23:47:14 +0530

    chapter6

---
+++ chapter-06/ex-6.5.c
@@ -0,0 +1,2 @@
+/************************************************************************** Ex-6.5. Write a function undef that will remove a name and definition from the table maintained by lookup and install.
+***************************************************************************/
